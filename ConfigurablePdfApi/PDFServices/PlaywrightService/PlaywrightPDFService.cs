c6ddee45b09ca0c0ec3a961bf86c10f6495c09e1
seba <s.zellah10@gmail.com>
2025-10-11 14:46:05 +0200

    Moved playwright browser into singleton

--- ConfigurablePdfApi/PDFServices/PlaywrightService/PlaywrightPDFService.cs
+++ ConfigurablePdfApi/PDFServices/PlaywrightService/PlaywrightPDFService.cs
@@ -4,33 +4,39 @@ using Microsoft.Playwright;
 
 namespace ConfigurablePdfApi.PDFServices.PlaywrightService;
 
-public class PlaywrightPDFService(
-    IServiceProvider serviceProvider, 
-    ILoggerFactory loggerFactory) : IPDFService
+public class PlaywrightPDFService : IPDFService
 {
-    
+    private static IPlaywright? _playwright;
+    private static IBrowser? _browser;
     public async Task<byte[]> GeneratePDF(string html)
     {
-        Microsoft.Playwright.Program.Main(["install"]);
+        //Microsoft.Playwright.Program.Main(["install"]);
         // return;
 
-        await using var htmlRenderer = new HtmlRenderer(serviceProvider, loggerFactory);
+        if (_browser is null)
+            await InitPlaywrightAsync();
+
+        if (_browser is null) return [];
         
-        using var playwright = await Playwright.CreateAsync();
-        var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-        {
-            Headless = true
-        });
-        
-        var page = await browser.NewPageAsync();
+        var page = await _browser.NewPageAsync();
         await page.SetContentAsync(html);
 
-        var pdfBytes =await page.PdfAsync(new PagePdfOptions
+        var pdfBytes = await page.PdfAsync(new PagePdfOptions
         {
             Format = "A4"
         });
         
         await page.CloseAsync();
-        return pdfBytes; 
+        return pdfBytes;
+
+    }
+    
+    static async Task InitPlaywrightAsync()
+    {
+        _playwright = await Playwright.CreateAsync();
+        _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = true
+        });
     }
 }
