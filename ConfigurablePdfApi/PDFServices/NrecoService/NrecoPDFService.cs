cdfab6cd93bc3273106718ddcabcbcef4b4dfe5a
seba <s.zellah10@gmail.com>
2025-10-16 20:37:42 +0200

    Add linux specific version

--- ConfigurablePdfApi/PDFServices/NrecoService/NrecoPDFService.cs
+++ ConfigurablePdfApi/PDFServices/NrecoService/NrecoPDFService.cs
@@ -1,14 +1,27 @@
 ﻿using ConfigurablePdfApi.PDFServices.Interfaces;
+using NReco.PdfGenerator;
 
 namespace ConfigurablePdfApi.PDFServices.NrecoService;
 
 public class NrecoPDFService : IPDFService
 {
+    private readonly HtmlToPdfConverter _converter;
+
+    public NrecoPDFService(IConfiguration configuration)
+    {
+        var licenseKey = Environment.GetEnvironmentVariable("NRECO_PDF_LICENSE_KEY");
+
+        _converter = new HtmlToPdfConverter();
+
+        if (!string.IsNullOrWhiteSpace(licenseKey))
+            _converter.License.SetLicenseKey("DEMO", licenseKey);
+        else
+            throw new Exception("No licence key");
+    }
+
     public Task<byte[]> GeneratePDF(string html)
     {
-        var htmlToPdf = new NReco.PdfGenerator.HtmlToPdfConverter();
-        var pdfBytes = htmlToPdf.GeneratePdf(html);
-
+        var pdfBytes = _converter.GeneratePdf(html);
         return Task.FromResult(pdfBytes);
     }
 }
